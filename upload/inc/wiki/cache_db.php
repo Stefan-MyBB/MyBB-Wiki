bb667eeb25dc74ec58f37b8ea205674f98860e29
Stefan-ST <544140+Stefan-MyBB@users.noreply.github.com>
2011-01-02 15:02:03 +0100

    format

--- upload/inc/wiki/cache_db.php
+++ upload/inc/wiki/cache_db.php
@@ -1,9 +1,23 @@
 <?php
-/*
-	Wiki-System
-	(c) 2007-2010 by StefanT
-	http://www.mybbcoder.info
-*/
+ /**
+ * This file is part of MyBB-Wiki.
+ * Copyright (C) 2007-2011 StefanT (http://www.mybbcoder.info)
+ * https://github.com/Stefan-ST/MyBB-Wiki
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 class wiki_cache
 {
 	private $core;
